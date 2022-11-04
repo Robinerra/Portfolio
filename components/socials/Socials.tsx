bc0d483042761ce8425c4c3b36ebe0c4f4c4d1f5
Robinerra <greggoryg@protonmail.com>
2022-11-04 16:51:33 -0700

    Add robots.txt, accessibility to anchor tags

--- components/socials/Socials.tsx
+++ components/socials/Socials.tsx
@@ -6,8 +6,8 @@ import Button from '../reveal_button/Button'
 function Socials() {
     return (
         <div className={style.container}>
-            <a href="https://github.com/Robinerra" rel="noreferrer" target={"_blank"}> <IconBrandGithub /> </a>
-            <a href="https://www.linkedin.com/in/elise-willar-96a743254/" rel="noreferrer" target={"_blank"}> <IconBrandLinkedin /> </a>
+            <a href="https://github.com/Robinerra" rel="noreferrer" target={"_blank"} aria-label="Link to my Github"> <IconBrandGithub /> </a>
+            <a href="https://www.linkedin.com/in/elise-willar-96a743254/" rel="noreferrer" target={"_blank"} aria-label="Link to my Linkeidin"> <IconBrandLinkedin /> </a>
             <p className={style.separate}>/</p>
             <Button url="./resume.pdf" newtab={true} text="Resume" iconName="IconFileDownload" iconSize="16" iconColor='white' />
         </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,10 @@ export default function Document() {
     //This is a lesser known feature of next.js I leverged to remove that issue of a white flash when reloading the page
     //because the CSS for the dark background hasn't been loaded yet. Helps prevent eyestrain.
     // NOTE: you may still get a flash of white when running on the devserver, this is just a dev server thing and it won't show in production.
-    <Html>
+    <Html lang="en">
       <Head>
-        <style> {"body {background-color:#121619;}"}
+        <style>
+          {"body {background-color:#121619;}"}
         </style>
 
 
@@ -19,7 +20,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-      
+
 
       </body>
     </Html>
