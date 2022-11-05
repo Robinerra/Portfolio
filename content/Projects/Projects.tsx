42167590a1219caf325407aa37b218b7d53520c6
Robinerra <greggoryg@protonmail.com>
2022-11-04 19:58:45 -0700

    Fix image directory

--- content/Projects/Projects.tsx
+++ content/Projects/Projects.tsx
@@ -16,7 +16,7 @@ function Projects() {
           <div className={style.projectsContainer}>
             <Project name="elisewillar.com" description='A portfolio using NextJS and React.' image="/images/projects/portfolio.png" link="elisewillar.com" github="https://github.com/Robinerra/Portfolio" tech={["devicon-nextjs-original", "devicon-react-original colored", "devicon-typescript-plain colored"]}  />
             
-            <Project name="More to come..." description='The journey has just now begun.' image="/images/project.png" />
+            <Project name="More to come..." description='The journey has just now begun.' image="/images/projects/project.png" />
            
             
           </div>
