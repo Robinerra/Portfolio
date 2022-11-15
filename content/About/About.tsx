f2645f03fa568dd2f829f981074d34e93db2ec04
Robinerra <greggoryg@protonmail.com>
2022-11-14 19:57:56 -0800

    Add bingopup project, add learning category

--- content/About/About.tsx
+++ content/About/About.tsx
@@ -20,17 +20,28 @@ function About() {
             It's 5 years later in 2022 and I've realized this is what I want to do for a living, something I love and that I'm passionate in.
             <br />            <br />
 
-            I work comfortably with:
+            I'm good at:
           </p>
           <ul className={style.skills}>
             <TechIcon icon="devicon-nodejs-plain colored" name="NodeJS" link="https://nodejs.org/en/" />
             <TechIcon icon="devicon-typescript-plain colored" name="Typescript" link="https://www.typescriptlang.org/" />
             <TechIcon icon="devicon-react-original colored" name="React" link="https://reactjs.org/" />
             <TechIcon icon="devicon-nextjs-original" name="NextJS" link="https://nextjs.org/" />
-            <TechIcon icon="devicon-postgresql-plain colored" name="Postgresql" link="https://www.postgresql.org/" />
+            <TechIcon icon="devicon-html5-plain colored" name="HTML" link="https://html.spec.whatwg.org/" />
+            <TechIcon icon="devicon-css3-plain colored" name="CSS" link="https://www.w3.org/Style/CSS/Overview.en.html" />
             <TechIcon icon="devicon-express-original" name="Express" link="https://expressjs.com/" />
+            <TechIcon icon="devicon-postgresql-plain colored" name="Postgresql" link="https://www.postgresql.org/" />
+            <TechIcon icon="devicon-mongodb-plain colored" name="MongoDB" link="https://www.mongodb.com/" />          </ul>
+          <p>I'm learning:</p>
+
+          <ul className={style.skills}>
+            <TechIcon icon="devicon-vuejs-plain colored" name="Vue" link="https://vuejs.org/" />
+            <TechIcon icon="devicon-php-plain colored" name="PHP" link="https://www.php.net/" />
+            <TechIcon icon="devicon-adonisjs-original" name="Astro" link="https://reactjs.org/" />
+
 
           </ul>
+
           <p>But don't worry if this doesn't align perfectly with your techstack, i'm a very fast and eager learner.</p>
         </div>
       </div>
--- content/Projects/Projects.tsx
+++ content/Projects/Projects.tsx
@@ -15,8 +15,11 @@ function Projects() {
 
           <div className={style.projectsContainer}>
             <Project name="elisewillar.com" description='A portfolio using NextJS and React.' image="/images/projects/portfolio.png" link="elisewillar.com" github="https://github.com/Robinerra/Portfolio" tech={["devicon-nextjs-original", "devicon-react-original colored", "devicon-typescript-plain colored"]}  />
-            
-            <Project name="More to come..." description='The journey has just now begun.' image="/images/projects/project.png" />
+            <Project name="bingopup.com" description='(Work in progress) A website for making and sharing bingo sheet memes.' image="/images/projects/bingopup.png" link="" github="" tech={["devicon-nextjs-original", "devicon-react-original colored", "devicon-typescript-plain colored", "devicon-postgresql-plain colored", "devicon-express-original"]}  />
+
+
+
+            <Project name="More to come..." description='' image="/images/projects/project.png" />
            
             
           </div>
